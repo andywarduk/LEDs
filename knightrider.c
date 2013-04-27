b8c935d3f845c247327de81ab4027ad07421efd0
Andrew Ward <ajw@gbh-warda-mac.local>
2013-04-27 19:31:47 +0100

    Tidy ups

--- knightrider.c
+++ knightrider.c
@@ -5,8 +5,9 @@
 #include "leds.h"
 
 #define QUANTA 500
+#define PATTERNBITS 12
+#define LEDS 5
 
-unsigned int patternbits = 12;
 unsigned int bitpatterns[] = {
     0x000, // 0000 0000 0000, 0 bits
     0x020, // 0000 0010 0000, 1 bit
@@ -25,7 +26,6 @@ unsigned int phase = 0;
 
 void onfor(int led, int r, int g, int b, useconds_t usecs);
 
-
 int main()
 {
     unsigned int position = 0;
@@ -37,12 +37,12 @@ int main()
     unsigned int r;
     unsigned int g;
     unsigned int b;
-    leddef ledstate[5];
+    leddef ledstate[LEDS];
 
     if (!led_init()) exit(-1);
 
     // Initialise the state
-    for (i = 0; i < 5; i++) {
+    for (i = 0; i < LEDS; i++) {
         ledstate[i].r = 0;
         ledstate[i].g = 0;
         ledstate[i].b = 0;
@@ -50,19 +50,19 @@ int main()
 
     while (1) {
         // Set up led state
-        for (i = 0; i < 5; i++) {
+        for (i = 0; i < LEDS; i++) {
             ledstate[i].r = 0;
         }
-        for (intensity = 3, i = position; intensity > 0 && i >= 0 && i < 5; intensity--, i -= direction) {
+        for (intensity = 3, i = position; intensity > 0 && i >= 0 && i < LEDS; intensity--, i -= direction) {
             ledstate[i].r = intensity;
         }
 
         // Display for one tenth second
-        for (j = 0; j < (80000 / (QUANTA * 5 * patternbits)); j++) {
+        for (j = 0; j < (80000 / (QUANTA * LEDS * PATTERNBITS)); j++) {
             // For each phase
-            for (phase = 0; phase < patternbits; phase++) {
+            for (phase = 0; phase < PATTERNBITS; phase++) {
                 // For each led
-                for (i = 0; i < 5; i++) {
+                for (i = 0; i < LEDS; i++) {
                     r = (bitpatterns[ledstate[i].r] & (1 << phase) ? 1 : 0);
                     g = (bitpatterns[ledstate[i].g] & (1 << phase) ? 1 : 0);
                     b = (bitpatterns[ledstate[i].b] & (1 << phase) ? 1 : 0);
@@ -73,7 +73,7 @@ int main()
 
         // Move the blob
         if (position == 0) direction = 1;
-        if (position == 4) direction = -1;
+        if (position == LEDS-1) direction = -1;
         position += direction;
     }
 
