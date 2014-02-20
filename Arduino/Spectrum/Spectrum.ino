4c8381f24368b92df728a68cade2a9af8e02986b
Andy Ward <andy.ward.uk@gmail.com>
2014-02-20 22:40:31 +0000

    use LEDs lib

--- Arduino/Spectrum/Spectrum.ino
+++ Arduino/Spectrum/Spectrum.ino
@@ -1,9 +1,11 @@
 // Sets LEDs to increasing intensity cycling through all colour combinations
 
+#include "LEDs.h"
+
 
 // Defines
 
-//#define DEBUG 1
+// #define DEBUG 1
 
 #define offsetof(st, m) __builtin_offsetof(st, m)
 //#define offsetof(st, m) ((size_t)(&((st *)0)->m))
@@ -11,45 +13,27 @@
 
 // Constants
 
-// Pins
-const int led1 = 3;
-const int led2 = 4;
-const int led3 = 5;
-const int led4 = 6;
-const int led5 = 7;
-
-const int red = 9;
-const int green = 10;
-const int blue = 11;
-
-// Pin array
-const int noleds = 5;
-
-const int ledpins[noleds]= {
-  led1, led2, led3, led4, led5
-};
-
 // Timing
-const int quanta = 1000; // ms per led
-const int noframes = 4; // number of frames to hold pattern for
+const int quanta = 10; // microseconds per led per %
+const int noFrames = 4; // number of frames to hold pattern for
 
 // Intensities
 const int intensities[] = {
   0, 1, 4, 25, 100
 };
 
-const int nointensity = (sizeof(intensities) / sizeof(int));
+const int noIntensity = (sizeof(intensities) / sizeof(int));
 
-const int intensityvalslong[] = {
+const int intensityValsLong[] = {
   0, 1, 1, 2, 2, 3, 3, 4, 4, 3, 3, 2, 2, 1, 1, 0
 };
 
-const int intensityvalsshort[] = {
+const int intensityValsShort[] = {
   0, 1, 2, 3, 4, 3, 2, 1
 };
 
-const int nointensityvalslong = (sizeof(intensityvalslong) / sizeof(int));
-const int nointensityvalsshort = (sizeof(intensityvalsshort) / sizeof(int));
+const int noIntensityValsLong = (sizeof(intensityValsLong) / sizeof(int));
+const int noIntensityValsShort = (sizeof(intensityValsShort) / sizeof(int));
 
 
 // Variables
@@ -59,16 +43,13 @@ typedef struct
   int r;
   int g;
   int b;
-} leddef;
+} ledDef;
 
-leddef ledstruct[noleds];
+ledDef ledStruct[LEDs::noLeds];
 
-leddef *leds[noleds];
+ledDef *leds[LEDs::noLeds];
 
-
-// Prototypes
-
-void setLed(int led, int r, int g, int b);
+LEDs ledsClass;
 
 
 // Functions
@@ -83,68 +64,56 @@ void setup()
   Serial.println("---=== Spectrum ===---");
   
   // Dump led pins
-  Serial.print("ledpins["); Serial.print(noleds); Serial.print("] = {");
-  for(i=0; i<noleds; i++){
+  Serial.print("ledPins["); Serial.print(LEDs::noLeds); Serial.print("] = {");
+  for(i=0; i<LEDs::noLeds; i++){
      if(i > 0) Serial.print(", ");
-     Serial.print(ledpins[i]);
+     Serial.print(LEDs::ledPins[i]);
   }
   Serial.println("}");
   
   // Dump RGB pins
-  Serial.print("red = "); Serial.println(red);
-  Serial.print("green = "); Serial.println(green);
-  Serial.print("blue = "); Serial.println(blue);
+  Serial.print("red = "); Serial.println(LEDs::red);
+  Serial.print("green = "); Serial.println(LEDs::green);
+  Serial.print("blue = "); Serial.println(LEDs::blue);
   
   // Print timing details
   Serial.print("LED quanta = "); Serial.print(quanta); Serial.println(" microseconds");
-  Serial.print("LED frame = "); Serial.print(quanta * noleds); Serial.println(" microseconds");
-  Serial.print("pattern frames = "); Serial.println(noframes);
-  Serial.print("LED pattern length = "); Serial.print(quanta * noleds * noframes); Serial.println(" microseconds");
+  Serial.print("LED PWM quanta = "); Serial.print(quanta * 100); Serial.println(" microseconds");
+  Serial.print("LED frame = "); Serial.print(quanta * 100 * LEDs::noLeds); Serial.println(" microseconds");
+  Serial.print("pattern frames = "); Serial.println(noFrames);
+  Serial.print("LED pattern length = "); Serial.print(quanta * 100 * LEDs::noLeds * noFrames); Serial.println(" microseconds");
 
   // Dump intensities
-  Serial.print("intensities["); Serial.print(nointensity); Serial.print("] = {");
-  for(i=0; i<nointensity; i++){
+  Serial.print("intensities["); Serial.print(noIntensity); Serial.print("] = {");
+  for(i=0; i<noIntensity; i++){
      if(i > 0) Serial.print(", ");
      Serial.print(intensities[i]);
      Serial.print("%");
   }
   Serial.println("}");
 
-  Serial.print("intensityvalslong["); Serial.print(nointensityvalslong); Serial.print("] = {");
-  for(i=0; i<nointensityvalslong; i++){
+  Serial.print("intensityValsLong["); Serial.print(noIntensityValsLong); Serial.print("] = {");
+  for(i=0; i<noIntensityValsLong; i++){
      if(i > 0) Serial.print(", ");
-     Serial.print(intensityvalslong[i]);
+     Serial.print(intensityValsLong[i]);
   }
   Serial.println("}");
 
-  Serial.print("intensityvalsshort["); Serial.print(nointensityvalsshort); Serial.print("] = {");
-  for(i=0; i<nointensityvalsshort; i++){
+  Serial.print("intensityValsShort["); Serial.print(noIntensityValsShort); Serial.print("] = {");
+  for(i=0; i<noIntensityValsShort; i++){
      if(i > 0) Serial.print(", ");
-     Serial.print(intensityvalsshort[i]);
+     Serial.print(intensityValsShort[i]);
   }
   Serial.println("}");
 #endif
 
-  // Set the LED pins as outputs, initialise the LED structs and pointers.
-  for(i=0; i<noleds; i++){
-    pinMode(ledpins[i], OUTPUT);  
-    digitalWrite(ledpins[i], HIGH);
-
-    leds[i] = &ledstruct[i];
-    ledstruct[i].r = 0;
-    ledstruct[i].g = 0;
-    ledstruct[i].b = 0;
-  }  
-
-  // Set the colour pins as outputs
-  pinMode(red, OUTPUT);     
-  pinMode(green, OUTPUT);     
-  pinMode(blue, OUTPUT);       
-
-  // Initialise the colour pins
-  digitalWrite(red, LOW);
-  digitalWrite(green, LOW);
-  digitalWrite(blue, LOW);
+  // initialise the LED structs and pointers.
+  for(i=0; i<LEDs::noLeds; i++){
+    leds[i] = &ledStruct[i];
+    ledStruct[i].r = 0;
+    ledStruct[i].g = 0;
+    ledStruct[i].b = 0;
+  }
 }
 
 
@@ -159,10 +128,10 @@ void loop()
   int l1;
   int l2;
   int l3;
-  byte *statebase;
+  byte *stateBase;
   int i;
   int l;
-  leddef *tmpled;
+  ledDef *tmpLed;
 
   for(stage=0; stage<4; stage++){
 #ifdef DEBUG
@@ -171,19 +140,19 @@ void loop()
 
     switch(stage){
     case 0:
-      p1 = offsetof(leddef, b);
-      p2 = offsetof(leddef, g);
-      p3 = offsetof(leddef, r);
+      p1 = offsetof(ledDef, b);
+      p2 = offsetof(ledDef, g);
+      p3 = offsetof(ledDef, r);
       break;
     case 1:
-      p1 = offsetof(leddef, g);
-      p2 = offsetof(leddef, r);
-      p3 = offsetof(leddef, b);
+      p1 = offsetof(ledDef, g);
+      p2 = offsetof(ledDef, r);
+      p3 = offsetof(ledDef, b);
       break;
     case 2:
-      p1 = offsetof(leddef, r);
-      p2 = offsetof(leddef, b);
-      p3 = offsetof(leddef, g);
+      p1 = offsetof(ledDef, r);
+      p2 = offsetof(ledDef, b);
+      p3 = offsetof(ledDef, g);
       break;
     }
 
@@ -193,87 +162,31 @@ void loop()
     Serial.print("p3="); Serial.print(p3); Serial.println();
 #endif
 
-    for(l1=0; l1<nointensityvalsshort; l1++){
+    for(l1=0; l1<noIntensityValsShort; l1++){
 #ifdef DEBUG
       Serial.print("l1="); Serial.print(l1); Serial.println();
 #endif
-      for(l2=0; l2<nointensityvalsshort; l2++){
-        for(l3=0; l3<nointensityvalslong; l3++){
-          statebase = (byte *) leds[noleds - 1];
-          *((int *)(statebase + p1)) = intensities[intensityvalsshort[l1]];
-          *((int *)(statebase + p2)) = intensities[intensityvalsshort[l2]];
-          *((int *)(statebase + p3)) = intensities[intensityvalslong[l3]];
+      for(l2=0; l2<noIntensityValsShort; l2++){
+        for(l3=0; l3<noIntensityValsLong; l3++){
+          stateBase = (byte *) leds[LEDs::noLeds - 1];
+          *((int *)(stateBase + p1)) = intensities[intensityValsShort[l1]];
+          *((int *)(stateBase + p2)) = intensities[intensityValsShort[l2]];
+          *((int *)(stateBase + p3)) = intensities[intensityValsLong[l3]];
 
-          for(i=0; i<noframes; i++){
-            for(l=0; l<noleds; l++){
-              setLed(l, leds[l]->r, leds[l]->g, leds[l]->b);
+          for(i=0; i<noFrames; i++){
+            for(l=0; l<LEDs::noLeds; l++){
+              ledsClass.setLed(l, leds[l]->r, leds[l]->g, leds[l]->b, quanta);
             }
           }
           
           // Move
-          tmpled = leds[0];
-          for (i = 0; i < noleds - 1; i++) {
+          tmpLed = leds[0];
+          for (i = 0; i < LEDs::noLeds - 1; i++) {
             leds[i] = leds[i+1];
           }
-          leds[noleds - 1] = tmpled;
+          leds[LEDs::noLeds - 1] = tmpLed;
         }
       }
     }
   }
 }
-
-
-void setLed(int led, int r, int g, int b)
-{
-  int i;
-  int rc;
-  int gc;
-  int bc;
-
-  // Initialise the rgb pins
-  digitalWrite(red, LOW);
-  digitalWrite(green, LOW);
-  digitalWrite(blue, LOW);
-
-  // Switch on the required led
-  digitalWrite(ledpins[led], LOW);
-
-  rc=0;
-  gc=0;
-  bc=0;  
-  for(i=0; i<100; i++){
-    rc+=r;
-    if(rc>=100){
-      digitalWrite(red, HIGH);
-      rc-=100;
-    }
-    else{
-      digitalWrite(red, LOW);
-    }
-
-    gc+=g;
-    if(gc>=100){
-      digitalWrite(green, HIGH);
-      gc-=100;
-    }
-    else{
-      digitalWrite(green, LOW);
-    }
-
-    bc+=b;
-    if(bc>=100){
-      digitalWrite(blue, HIGH);
-      bc-=100;
-    }
-    else{
-      digitalWrite(blue, LOW);
-    }
-
-    // Wait
-    delayMicroseconds(quanta / 100);
-  }
-
-  // Switch off the led
-  digitalWrite(ledpins[led], HIGH);  
-}
-
